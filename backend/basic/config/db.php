e550b8fbb2873e06c56c79f0065273f536605827
gisehweb <gisehbc@gmail.com>
2022-07-11 19:43:02 -0300

    tp2 punto2 se activa el modulo gii y se comprueba su funcionamiento

--- backend/basic/config/db.php
+++ backend/basic/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => "pgsql:host=" . $_ENV['DATABASE_HOST'] . ";port=5432;dbname=" . $_ENV['DATABASE_NAME'],
     'username' => $_ENV['DATABASE_USER'],
-    'password' => $_ENV['DATABASE_PASSWORD'],
+    'password' => $_ENV['DATABASE_PASS'],
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
