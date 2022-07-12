5116441706f1bb4b4aa4e4285996af72c0693dd1
gisehweb <gisehbc@gmail.com>
2022-07-12 17:02:12 -0300

    tp2 punto 6 Se crearon las tablas reserva_aula y horario_materia

---
+++ backend/basic/migrations/m220712_194504_crear_table_reserva_aula.php
@@ -0,0 +1,53 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220712_194504_crear_table_reserva_aula
+ */
+class m220712_194504_crear_table_reserva_aula extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%reserva_aula}}', [
+            'id' => $this->primaryKey(),
+            'id_aula' => $this->integer()->notNull(),
+            'fh_desde' => $this->timestamp(),
+            'fh_hasta' => $this->timestamp(),
+            'observacion' => $this->string(256)->notNull()
+        ]);
+
+        $this->createIndex('idx-reserva_aula-id_aula','reserva_aula', 'id_aula');
+
+        $this->addForeignKey('fk-reserva_aula-id_aula', 'reserva_aula', 'id_aula', 'aula', 'id', 'CASCADE');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk-reserva_aula-id_aula', 'reserva_aula');
+        $this->dropIndex('idx-reserva_aula-id_aula','reserva_aula');
+
+        $this->dropTable('{{%reserva_aula}}');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220712_194504_crear_table_reserva_aula cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
