9796dc1da0fc98981c8a3d295226369260bd1fe6
gisehweb <gisehbc@gmail.com>
2022-07-19 17:57:15 -0300

    TP4 punto 7 Se instalo la libreria VueRouter- Se creo en plugins router.js en component AulasAbout y AulasHome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import custom_axios from "@/plugins/axios";
 import VueAxios from "vue-axios";
+import router from './plugins/router'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, custom_axios);
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
